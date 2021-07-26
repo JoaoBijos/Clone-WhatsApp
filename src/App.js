ecd0e7f1b83a3361cfef0607a60d0560c3107599
joaobijos96 <joaopedrobijos96@gmail.com>
2021-07-26 16:29:28 -0300

    feat02(sidebar): creating the sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+
+import ChatList from './components/ChatList/index'
+
 export default () => {
+
+  const [chatlist, setChatList] = useState([{},{},{},{}]);
+
   return (
     <div className="app-window">
       <div className='sidebar'>
-        ...
+        <header>
+          <img classname='header--avatar' src='https://www.w3schools.com/howto/img_avatar2.png' alt=''/>
+          <div className='header--buttons'>
+            <div className='header--btn'>
+              <DonutLargeIcon style={{color:'#919191'}}/>
+            </div>
+            <div className='header--btn'>
+              <ChatIcon style={{color:'#919191'}}/>
+            </div>
+            <div className='header--btn'>
+              <MoreVertIcon style={{color:'#919191'}}/>
+            </div>
+          </div>
+        </header>
+        <div className='search'>
+          <div className='search--input'>
+            <SearchIcon font-size='small' style={{color:'#919191'}}/>
+            <input type='search' placeholder='Procurar ou começar uma nova conversa'/>
+          </div>
+        </div>
+        <div className='chatlist'>
+          {chatlist.map((item, key)=>{
+            <ChatList
+              key={key}
+            />
+          })}
+        </div>
       </div>
       <div className='contentarea'>
         ...  
